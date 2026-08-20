357a18069af63ea6b0d636974f5e2984132f0fb7
JordanUrbaezLu <93447469+JordanUrbaezLu@users.noreply.github.com>
2026-08-20 09:37:24 +0000

    Refresh baked battle data (latest Smogon month)

--- public/sw.js
+++ public/sw.js
@@ -16,7 +16,7 @@
  * `activate` deletes the previous version's caches. Do not edit VERSION by hand.
  */
 
-const VERSION = "cpx-2cf667032d7f";
+const VERSION = "cpx-5fbccac5db6d";
 const STATIC_CACHE = `cpx-static-${VERSION}`;
 const IMAGE_CACHE = `cpx-images-${VERSION}`;
 const PAGE_CACHE = `cpx-pages-${VERSION}`;
